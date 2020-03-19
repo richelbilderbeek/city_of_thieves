4318e6676a174b9637b1ba7a40e6f582e7fa7194
Rafayel Gardishyan <rgardishyan@gmail.com>
2020-03-19 23:09:53 +0100

    Fixed some bugs: now it works perfectly and saves the tree to a json file

--- Article/rafayel/graph-decoder.py
+++ Article/rafayel/graph-decoder.py
@@ -1,15 +1,46 @@
 import re
+import json
 
 g_file = open('graph.gv', 'r')
 
-for line in g_file:
-    params = g_file.readline().strip()
+story_tree = {}
 
-    if re.search('([0-9]{1,3})\[label="([0-9]{1,3})" shape=([a-z])* color=([a-z])*\];', params):
-        node_nr = re.findall('[0-9]{1,3}', params)[0]
-        node_color = re.findall('color=([a-z]{3,5})', params)[0]
+for line in g_file:
+    params = line.strip()
+
+    if re.search(r'([0-9]{1,3})\[label="([0-9]{1,3})" shape=([a-z])* color=([a-z])*\];', params):
+        node_nr = re.findall(r'[0-9]{1,3}', params)[0]
+        node_color = re.findall(r'color=([a-z]{3,5})', params)[0]
+
+        # f = True
+        # for room in story_tree:
+        #     if room["id"] == node_nr:
+        #         f = False
+        
+        # if f:
+        story_tree[node_nr] = {
+            "id": node_nr,
+            "type": node_color,
+            "connections": []
+        }
+
         print("Node details: {}; {}".format(node_nr, node_color))
     
-    if re.search('([0-9]{1,3})->([0-9]{1,3}) \[ label="([a-zA-Z])*"\];', params):
-        node_nr = re.findall('[0-9]{1,3}', params)
+    if re.search(r'([0-9]{1,3})->([0-9]{1,3}) \[ label="([a-zA-Z])*"\];', params) or re.search(r'([0-9]{1,3})->([0-9]{1,3});', params):
+        node_nr = re.findall(r'[0-9]{1,3}', params)
+
+        if node_nr[0] in story_tree:
+            story_tree[node_nr[0]]["connections"].append(node_nr[1])
+
         print("Connection: from {} to {}".format(node_nr[0], node_nr[1]))
+
+    if re.search(r'([0-9]{1,3})-> GameOver;', params):
+        node_nr = re.findall(r'[0-9]{1,3}', params)[0]
+
+        if node_nr in story_tree:
+            story_tree[node_nr[0]]["connections"].append("GameOver")
+
+        print("GameOver: from {}".format(node_nr))
+
+with open('tree.json', 'w+') as tree_file:
+    tree_file.write(json.dumps(story_tree))
