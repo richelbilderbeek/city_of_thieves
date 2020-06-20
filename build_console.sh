d3e8808728226c685f3caebaa4b72abcf4e15d99
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-20 14:18:59 +0200

    Use 'make console' for console version

--- build_console.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./build_files.sh
-cd Console
-qmake -qt=qt5 CityOfThievesConsole.pro
-make --silent
--- build_console_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-./build_files.sh
-cd Console
-qmake CityOfThievesConsoleTest.pro
-make --silent debug
