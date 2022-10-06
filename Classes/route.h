69e788e93b7e12fe73ad8e6f6da5efe09b5109fb
richelbilderbeek <richel@richelbilderbeek.nl>
2022-10-06 14:47:14 +0200

    EOD

--- Classes/route.h
+++ Classes/route.h
@@ -2,9 +2,15 @@
 #define ROUTE_H
 
 #include <vector>
+
 /// Get the classical winning chapter sequence
 /// Returns a vector with 'v[i] = j',
 /// which indicates that at chapter i, go to chapter v[i]
 std::vector<int> GetWinningRoute() noexcept;
+
+/// Collect all routes that make sense
+std::vector<std::vector<int>> GetSaneRoutes() noexcept;
+
+
 
 #endif // ROUTE_H
